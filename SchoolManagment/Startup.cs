d6c635d8bee3bbba0fd7cecbaf9bf142ae5b5e40
tesfayheluface@gmail.com <tesfayheluface@gmail.com>
2021-09-06 22:46:58 +0300

    Add project files.

---
+++ SchoolManagment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolManagment.Startup))]
+namespace SchoolManagment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
